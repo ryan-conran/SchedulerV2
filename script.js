11e555a0764683b733f409c6f991d8e0d2fb6f95
Ryan Conran <ryan.conran2@gmail.com>
2020-11-02 15:21:25 -0600

    Initial Commit

---
+++ script.js
@@ -0,0 +1,10 @@
+$(function () {
+    let today = moment().format("dddd, MMMM Do");
+    $("p").append(today);
+
+    // create a function that will apply classes based off time
+
+    let currentTime= moment();
+    if ()
+
+})
