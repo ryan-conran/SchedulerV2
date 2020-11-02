9fe9d620494afbc5571170ee7b9c455c05a74b9c
Ryan Conran <ryan.conran2@gmail.com>
2020-11-02 17:45:04 -0600

    Made adjustments to the for loop

--- script.js
+++ script.js
@@ -4,39 +4,15 @@ $(function () {
 
     // create a function that will apply classes based off time
 
-    let currentTime= moment().format('H');
+    let currentTime= moment().format('h');
     console.log(currentTime);
-    for (i=9; i<= 17; i++) {
+    for (let i=9; i<= 17; i++) {
         if(i < currentTime) {
-            $("#row9").addClass("past");
-            $("#row10").addClass("past");
-            $("#row11").addClass("past");
-            $("#row12").addClass("past");
-            $("#row13").addClass("past");
-            $("#row14").addClass("past");
-            $("#row15").addClass("past");
-            $("#row16").addClass("past");
-            $("#row17").addClass("past");
+            $("#row" + i).addClass("past");
         } else if(i == currentTime) {
-            $("#row9").addClass("present");
-            $("#row10").addClass("present");
-            $("#row11").addClass("present");
-            $("#row12").addClass("present");
-            $("#row13").addClass("present");
-            $("#row14").addClass("present");
-            $("#row15").addClass("present");
-            $("#row16").addClass("present");
-            $("#row17").addClass("present");
-        } else( i > currentTime) ;{
-            $("#row9").addClass("future");
-            $("#row10").addClass("future");
-            $("#row11").addClass("future");
-            $("#row12").addClass("future");
-            $("#row13").addClass("future");
-            $("#row14").addClass("future");
-            $("#row15").addClass("future");
-            $("#row16").addClass("future");
-            $("#row17").addClass("future");
+            $("#row" + i).addClass("present");
+        } else if( i > currentTime) ;{
+            $("#row" + i).addClass("future");
         };
     };
 
